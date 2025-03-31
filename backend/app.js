dea21f31ee98958468dd05359d46f732b6571d10
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-03-31 19:17:32 +0100

    initialise testing and backend

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ message: 'Server running' });
+});
+
+module.exports = app;
