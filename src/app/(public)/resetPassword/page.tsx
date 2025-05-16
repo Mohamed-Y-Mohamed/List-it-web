e66d43e5a857b696efc9aa5d7ebdb9798651af9b
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-05-16 01:56:32 +0100

    note tick is centered, task remove date added and select collection improved for all browsers. nav seperated into two.

--- src/app/(public)/resetPassword/page.tsx
+++ src/app/(public)/resetPassword/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import React, { Suspense } from "react";
 import ResetPassword from "@/components/resetpassword/reset";
 
 export default function ResetPasswordPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ResetPassword />
-    </Suspense>
-  );
+  return <ResetPassword />;
 }
